d492adce7c30fc92a1cdb9e30fb9510c5e9e898a
matheus-estevam <matheusestevam17055@gmail.com>
2023-12-08 16:18:12 -0300

    Parte 1 da config do banco de dados firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,20 +11,28 @@ import { EventService } from './demo/service/event.service';
 import { IconService } from './demo/service/icon.service';
 import { NodeService } from './demo/service/node.service';
 import { PhotoService } from './demo/service/photo.service';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
 
 @NgModule({
-    declarations: [
-        AppComponent, NotfoundComponent
-    ],
+    declarations: [AppComponent, NotfoundComponent],
     imports: [
         AppRoutingModule,
-        AppLayoutModule
+        AppLayoutModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireDatabaseModule,
     ],
     providers: [
         { provide: LocationStrategy, useClass: HashLocationStrategy },
-        CountryService, CustomerService, EventService, IconService, NodeService,
-        PhotoService, ProductService
+        CountryService,
+        CustomerService,
+        EventService,
+        IconService,
+        NodeService,
+        PhotoService,
+        ProductService,
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    firebaseConfig: {
+        apiKey: 'AIzaSyB9RaVhZH8yC3gZR37_oPVyhMfu78K5bIs',
+        authDomain: 'crud-devops-afced.firebaseapp.com',
+        databaseURL: 'https://crud-devops-afced-default-rtdb.firebaseio.com',
+        projectId: 'crud-devops-afced',
+        storageBucket: 'crud-devops-afced.appspot.com',
+        messagingSenderId: '715503333168',
+        appId: '1:715503333168:web:50856638d091fade1d62f5',
+        measurementId: 'G-H8699CCPBE',
+    },
 };
 
 /*
